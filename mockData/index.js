4544bca5ac2d0e815ad101f6957a3eb92bee483b
LeviGao <levi.gao@nagarro.com>
2023-03-08 10:53:37 +0800

    order list

--- mockData/index.js
+++ mockData/index.js
@@ -1,2 +1,5 @@
 export * from './productList'
 export * from './shoppingCartList'
+export * from './orderList'
+export * from './orderDetail'
+
--- pages/order/index.js
+++ pages/order/index.js
@@ -1,18 +1,54 @@
 // pages/order/index.ts
+import {getOrderList} from '../../api/orderList'
 Page({
-
   /**
    * Page initial data
    */
   data: {
-
+    orderList:[],
+    orderStates:[
+      {
+        title: "全部",
+        name: "all",
+      },
+      {
+        title: "订单处理中",
+        name: "all2",
+      },
+      {
+        title: "进行中",
+        name: "all3",
+      },
+      {
+        title: "已预订",
+        name: "all4",
+      },
+      {
+        title: "不相关",
+        name: "all5",
+      },
+      {
+        title: "部分发货",
+        name: "all6",
+      },
+      {
+        title: "部分发货2",
+        name: "all7",
+      },
+    ], 
+    loading:false,
+    finished:false,
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad() {
-
+  async onLoad() {
+    const list = await getOrderList()
+    console.log("onLoad---------------");
+    this.setData({
+      orderList: list
+    })
   },
 
   /**
@@ -62,5 +98,16 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+
+  pageScroll: function (e) {
+    console.log('pageScroll', e.detail)
+  },
+
+  orderNoClick: function(options) {
+    console.log("orderNoClick--------------");
+    wx.navigateTo({
+      url: '../orderDetail/orderDetail'
+    })
+}
 })
